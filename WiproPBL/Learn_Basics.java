bf98299a184e66d9a71df31ec392ae6a7c33b679
RV0225 <83663322+RV0225@users.noreply.github.com>
2023-05-24 23:36:44 +0530

    stars_One

--- WiproPBL/Learn_Basics.java
+++ WiproPBL/Learn_Basics.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 public class Learn_Basics {
 
     public static void main(String args[]) throws IOException {
@@ -35,11 +37,123 @@ public class Learn_Basics {
         // }
 
         // BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        String gender = args[0];
-        int age = Integer.valueOf(args[1]);
-        if (gender.equals("Female") && age > 1 && age < 57) {
-            System.out.println("8.2%");
-        }
+        // String gender = args[0];
+        // int age = Integer.valueOf(args[1]);
+        // if (gender.equals("Female") && age > 1 && age < 57) {
+        // System.out.println("8.2%");
+        // }
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // // Scanner sc=new Scanner(System.in);
+        // char c = (char) br.read();
+        // if (Character.isLowerCase(c)) {
+        // System.out.println(Character.toUpperCase(c));
+        // } else if (Character.isUpperCase(c)) {
+        // System.out.println(Character.toLowerCase(c));
+        // }
+
+        // Map<Character,String>map=new
+        // HashMap<>(){{put('o',"orange");{put('b',"blue");}{put('g',"Green");}{put('w',"white");}};
+        // char key = (char) br.read();
+        // switch (key) {
+        // case 'b':
+        // System.out.println("Blue");
+        // break;
+        // case 'c':
+        // System.out.println("Cyan");
+        // break;
+        // case 'w':
+        // System.out.println("White");
+        // break;
+        // default:
+        // System.out.println("Invalid Choice For Color");
+        // break;
+        // }
+
+        // int n = br.read();
+
+        // for (int j = 10; j < 100; j++) {
+        // boolean flag = true;
+        // for (int i = 2; i <= Math.sqrt(n); i++) {
+        // if (j % i == 0) {
+        // flag = false;
+        // break;
+        // }
+        // }
+        // if (!flag) {
+        // System.out.println(j);
+        // }
+        // }
+        // if (n <= 1) {
+        // System.out.println("Not a Prime");
+        // }
+        // boolean flag = true;
+        // for (int i = 2; i <= Math.sqrt(n); i++) {
+        // if (n % i == 0) {
+        // flag = false;
+        // }
+        // }
+        // if (flag) {
+        // System.out.println("Yes It is a Prime Number");
+        // } else {
+        // System.out.println("Not a Prime");
+        // }
+
+        // int number = Integer.parseInt(br.readLine());
+        // // // System.out.println(number % 10);
+        // // int sum = 0;
+        // // while (number > 0) {
+        // // sum += number % 10;
+        // // // System.out.println(r);
+        // // // sum += r;
+        // // number /= 10;
+        // // }
+        // // System.out.println(sum);
+
+        // int t = number;
+        // int num = 0;
+        // while (t > 0) {
+        // int r = t % 10;
+        // num = num * 10 + r;
+        // t /= 10;
+        // }
+        // System.out.println(num);
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            // for Spaces
+            for (int j = 0; j < n - 1 - i; j++) {
+                System.out.print(" ");
+            }
+            // for stars
+            for (int k = 0; k < i + 1; k++) {
+                System.out.print("*");
+            }
+            // for stars
+            for (int k = 1; k < i + 1; k++) {
+                System.out.print("*");
+            }
+            // for spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < n; i++) {
+            // for spaces
+            for (int j = 1; j < i + 1; j++) {
+                System.out.print(" ");
+            }
+            // for stars
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("*");
+            }
+            // for stars
+            for (int j = n - 1 - i; j >= 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
---
+++ WiproPBL/notes.md
@@ -0,0 +1,26 @@
+# How to read a Single char in java
+
+`char c=(char)br.read();`
+
+# Character.toLowerCase ->changes the character
+
+#Character.isLowerCase -> check the character is lower or not
+
+# same for uppercase
+
+# code for prime number
+
+```for (int j = 10; j < 100; j++) {
+            boolean flag = true;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (j % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (!flag) {
+                System.out.println(j);
+            }
+        }
+
+```
