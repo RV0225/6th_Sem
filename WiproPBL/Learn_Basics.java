3ba2f07f69b567fd029ca4cc5ff7a134a05205a9
RV0225 <83663322+RV0225@users.noreply.github.com>
2023-05-24 21:06:34 +0530

    change name

---
+++ WiproPBL/Learn_Basics.java
@@ -0,0 +1,37 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Learn_Basics {
+
+    public static void main(String args[]) throws IOException {
+        // if (args.length == 0) {
+        // System.out.println("No Command Line Argument");
+        // } else {
+        // String argsnew = String.join(" Technologies ", args);
+        // System.out.println(argsnew);
+        // }
+
+        // int sum = 0;
+        // for (String s : args) {
+        // sum += Integer.parseInt(s);
+        // }
+        // System.out.println(sum);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        PriorityQueue<Character> pq = new PriorityQueue<>();
+        for (char c : s.toCharArray()) {
+            pq.add(c);
+        }
+        System.out.println(pq.poll() + "," + pq.poll());
+        // int a = 56;
+        // int b = 7;
+        // if (a % 10 == b % 10) {
+        // System.out.println("Same");
+        // } else {
+        // System.out.println("Different");
+        // }
+    }
+}
